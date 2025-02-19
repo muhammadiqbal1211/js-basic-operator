9a5c96addab75b7344e5705dc3be4444490b26e0
Muhmmad Iqbal <iqbalm3409@gmial.com>
2025-02-20 00:26:51 +0500

    first commit

---
+++ JS BASIC IF ELSE/main.js
@@ -0,0 +1,66 @@
+// 1
+// let num = +prompt("Write any num")
+// if(num==1){
+//     console.log("It is a positive");
+// }
+// else if(num==-1){
+// console.log("It is a neg num");
+// }
+// else{
+//     console.log("It is a zero num");
+// }
+// 2
+// let votingEligiblitiy = +prompt("How many are you old ? ")
+// if (votingEligiblitiy>=18){
+//  console.log("you can vote");
+// }
+// else{
+//     console.log("you cann't do vote ");
+// }
+// 3
+// let divisibility = + prompt("write any num")
+// if(divisibility/5){
+// console.log("it num can be divide");
+// }
+// else{
+//     console.log("it num is can not be divide");
+// }
+// 4
+// let a = +prompt("write name of leap year")
+// let leapYear =2023
+// if(a===leapYear){
+// console.log("you are right");
+// }
+// else{
+//    console.log( "you are wrong");
+// }
+// 5 
+// let a = +prompt("Enter first number:");
+// let b = +prompt("Enter second number:");
+// let c = +prompt("Enter third number:");
+
+// if (a >= b && a >= c) {
+//     console.log("The largest number is:", a);
+// } else if (b >= a && b >= c) {
+//     console.log("The largest number is:", b);
+// } else {
+//     console.log("The largest number is:", c);
+// }
+// let age = +prompt("write your age");
+// if(age=12){
+//     console.log("you are child");
+// }
+// else if (age>12 , age<=18){
+// console.log("you are teenager");
+// }
+// else {
+//     console.log("you are adult");
+// }
+let a = +prompt("write any alphabet")
+let vowel = a && e &&  i && o && u 
+if(a===vowel){
+    console.log("this is a vowel ");
+}
+else{
+console.log("this is a constant");
+}
---
+++ JS BASIC OPeraTOR/main.js
@@ -0,0 +1,35 @@
+// 1
+// let add = 2+3
+// console.log(add)
+// 2
+// let a =25%2
+// console.log(a);
+// 3 
+// let a = 3>4
+// console.log(a);
+// 4 
+// let a = 1
+// let b = a++
+// console.log(a);
+// 5
+// let a = 3
+// a--
+// console.log(a);
+// 6
+// let areaOfRec = 45*65
+// console.log(areaOfRec);
+// 7 
+// let a = 1
+// a+=10
+// console.log(a);
+// 8
+// let simpleInterest = (12*5*4)/100
+// console.log(simpleInterest);
+// 9 
+// let findReminder = 25%4
+// console.log(findReminder);
+// 10
+// let a  = 3 ; let b = 4
+// console.log(a===b);
+// let c = 3; 
+// console.log(a===c);
